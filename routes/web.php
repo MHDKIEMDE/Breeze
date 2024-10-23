bdb7275a9c2b7c4ad10e48e0aa899b33446c3756
MHDKIEMDE <mkiemde00@gmail.com>
2024-10-23 17:10:25 +0000

    Add the multiple auth

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/client/dashboard', [ClientController::class, 'index'])->middleware(['auth', 'verified', 'rolemanager:coustomer'])->name('dashboard');
+Route::get('/user/dashboard', [UserController::class, 'index'])->middleware(['auth', 'verified', 'rolemanager:user'])->name('user');
+Route::get('/admin/dashboard', [AdminController::class, 'index'])->middleware(['auth', 'verified', 'rolemanager:admin'])->name('admin');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +20,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
